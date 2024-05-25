8cef74f227a997c0e2b97d8bc009a4ba9c1aeb50
Tarun Kumar S <srfashions.tarun@gmail.com>
2024-05-25 14:23:53 +0530

    add folder

--- tarun/test1.py
+++ tarun/test1.py
@@ -2,6 +2,6 @@ import ezgmail, os
 from transformers import pipeline
 unreadThreads = ezgmail.unread()
 # ezgmail.summary(unreadThreads, )
-print(len(unreadThreads))
+# print(unreadThreads)
 print(unreadThreads[0].messages[0].body)
-summarizer = pipeline('summarization')
+# summarizer = pipeline('summarization')
